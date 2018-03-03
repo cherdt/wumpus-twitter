b5f4e47814e8b7a6024b274852afe5b8f6c94cdd
Chris Herdt <cherdt@gmail.com>
2018-03-02 23:52:51 -0600

    Fix #8 (needs to be triggered or cron'd)

---
+++ follow_unfollowed_followers.py
@@ -0,0 +1,25 @@
+import sys
+from config import get_client
+from birdy.twitter import UserClient
+from twitter import Twitter
+
+
+def main(argv):
+
+    # Make sure we received a target user
+    if len(argv) != 2:
+        print "usage: python follow_unfollowed_followers.py username"
+        sys.exit()
+
+    # create twitter client
+    client = get_client()
+
+    # create Twitter...
+    twitter = Twitter(client)
+
+    # follow unfollowed followers
+    twitter.follow_my_followers(argv[1])
+
+
+if __name__ == "__main__":
+    main(sys.argv)
--- twitter.py
+++ twitter.py
@@ -8,6 +8,7 @@ class Twitter:
         self.last_seen_dm = 1
 
     def get_last_seen_dm(self):
+    	"""Get the most recent tweet id from the state table"""
     	db = shelve.open(self.statedb)
     	try:
     	    self.last_seen_dm = db['last_seen_dm']
@@ -17,22 +18,44 @@ class Twitter:
     	return self.last_seen_dm
 
     def set_last_seen_dm(self, id):
+    	"""Update the most recent tweet id in the state table to the specified id"""
     	db = shelve.open(self.statedb)
     	db['last_seen_dm'] = id
     	db.close()
 
     def favorite(self, tweet_id):
+    	"""Favorite the specified tweet"""
         response = self.client.api.favorites.create.post(id=str(tweet_id))
 
     def retweet(self, tweet_id):
+    	"""Retweet the specified tweet"""
         response = self.client.api.statuses.retweet.post(id=str(tweet_id))
 
     def update(self, tweet):
+    	"""Post the specified tweet to the user's timeline"""
         response = self.client.api.statuses.update.post(status=str(tweet))
 
     def dm(self, user, message):
+    	"""Send the specified user the specified message as a DM"""
     	response = self.client.api.direct_messages.new.post(screen_name=str(user), text=str(message))
 
     def get_dms_since(self, id):
+    	"""Get direct messages older than the specified id"""
         response = self.client.api.direct_messages.get(since_id=str(id), count=1)
         return response
+
+    def get_followers(self, username):
+        """Get users who follow the specified screen_name"""
+        response = self.client.api.followers.list.get(screen_name=username, skip_status="true", include_user_entities="false")
+        return response
+
+    def follow(self, username):
+        """Follow the specified screen_name"""
+        response = self.client.api.friendships.create.post(screen_name=username,follow='true')
+
+    def follow_my_followers(self, username):
+        """Follow users that follow the specified user."""
+        response = self.get_followers(username)
+        for follower in response.data.users:
+            if not follower.following:
+                self.follow(follower.screen_name.encode('ascii', 'ignore'))
